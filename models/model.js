de73b06a7cc553a15f75a59578f132553c275f61
Ameen <ameenahmed1998@gmail.com>
2022-04-29 15:15:14 +0100

    Created model item schema

---
+++ models/model.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let ModelSchema = new Schema({
+    name:{type:String, required:true},
+    brand:{type:Schema.Types.ObjectId, ref:'Brand', required:true},
+    description:{type:String, required:true},
+    retail_price:{type:Number, required:true},
+    thumbnail:{type:String, required:false}
+})
+
+ModelSchema
+    .virtual('url')
+    .get(function() {
+        return '/catalog/model/' + this._id
+    });
+
+module.exports = mongoose.model('Model', ModelSchema)
