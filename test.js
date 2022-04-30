6499cb3d467f2b9797e2dc454f531c13b31e3063
Ameen <ameenahmed1998@gmail.com>
2022-04-30 17:47:43 +0100

    testing update

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 let SneaksAPI = require('sneaks-api');
+const sneaksRoutes = require('sneaks-api/routes/sneaks.routes');
 const sneaks = new SneaksAPI();
 
 
 sneaks.getMostPopular(100, function(err, products) {
     const brands = []
 
-    products.forEach()
+    products.forEach(product => {
+        if (!brands.includes(product._doc.brand.toString())) {
+            brands.push(product._doc.brand.toString())
+        }
+    })
+
+    console.log(brands)
+})
+
+sneaks.getMostPopular(100, function(err, products) {
+    
+    for (product of products) {
+        console.log(product._doc.brand.toString())
+    }
 })
